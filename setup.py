8509620ba0d2baf7ca77833aea6c45da4294f55b
Aniket Lotlikar <alotlikar98@gmail.com>
2024-04-03 23:33:25 +0530

    file structure completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Airline_Passenger_Classification",
+    version="0.0.1",
+    author="Aniket Lotlikar",
+    author_email="alotlikar98@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
